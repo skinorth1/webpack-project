885b3ad568e13fc4721766b231c5868ad1af32fb
skinorth <skinorth1@gmail.com>
2019-06-07 20:37:01 +0800

    [modify] webpack set up 'babel'

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/js/main.js',
+const config = {
+    entry: {
+        main: './src/js/main.js'
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist/js'),
         publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
     }
 };
+
+module.exports = config;
